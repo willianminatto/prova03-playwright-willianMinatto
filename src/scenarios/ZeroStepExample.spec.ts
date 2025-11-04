88b44b3707656114b628de1eaf3f006a149400e0
Willian Minatto <willian.279222@alunosatc.edu.br>
2025-11-04 19:35:38 -0300

    prova

--- src/scenarios/ZeroStepExample.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import { ai } from '@zerostep/playwright';
-
-test.skip('zerostep example', async ({ page }) => {
-  await page.goto('https://demo.automationtesting.in/Register.html');
-
-  const aiArgs = { page, test };
-  await ai('enter values in full name field', aiArgs);
-});
--- src/support/elements/BaseElements.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from '@playwright/test';
-
-export default abstract class BaseElements {
-  constructor(readonly page: Page) {
-    this.page = page;
-  }
-}
